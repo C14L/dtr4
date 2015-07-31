c76cce09502fc78572fc617fef318d7e87776608
Christian Steimel <me@C14L.com>
2015-07-31 11:49:13 +0200

    Clean up WSGI configuration

--- dtr4/wsgi.py
+++ dtr4/wsgi.py
@@ -5,45 +5,13 @@ WSGI config for dtr4 project.
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-#
-# WSGIPythonHome /opt/elligue/venv/dtr4/bin/python
-# WSGIDaemonProcess elligue4 python-path=/opt/elligue/dtr4:/opt/elligue/venv/dtr4/lib/python3.2/site-packages
-# WSGIProcessGroup elligue4
-# WSGIScriptAlias / /opt/elligue/dtr4/dtr4/wsgi.py process-group=elligue4
-#
-
 import os
 import sys
-#import site
 from django.core.wsgi import get_wsgi_application
-
-# Fallback to print some error message
-some_error = 'ALL OKAY!'
-def errapp(env, start_response):
-    from datetime import datetime
-    from django import get_version as django_version
-    global some_error
-    data = "<ul><li>{2}<li>Python: '{0}'<li>Django: '{3}'<li>some error happened: '{1}'</ul>".format(
-        sys.version, some_error, datetime.now(), django_version())
-    start_response("200 OK", [("Content-Type", "text/html")])
-    return [data]
-
-# Add the site-packages of the chosen virtualenv to work with
-#site.addsitedir('/opt/elligue/venv/dtr4/lib/python2.7/site-packages')
 
 # Add the app's directory to the PYTHONPATH
 sys.path.append('/opt/elligue/dtr4')
 sys.path.append('/opt/elligue/dtr4/dtr4')
-
 # Tell Django where to find the settings file
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dtr4.settings")
-
-# Activate your virtual environment
-#activate_env = "/opt/elligue/venv/dtr4/bin/activate_this.py"
-#execfile(activate_env, dict(__file__=activate_env))
-
-try:
-    application = get_wsgi_application()
-except Exception as e:
-    some_error = e
-    application = errapp
+application = get_wsgi_application()
